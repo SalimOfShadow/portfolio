9a08e7d27b33a53a509ed722dcc4beb825de7f13
Salim <152934001+SalimOfShadow@users.noreply.github.com>
2024-12-27 15:43:33 +0100

    fix WIP image size and animation on mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,19 @@
-import * as React from "react";
+import * as React from 'react';
 import {
   darkThemeStyles,
   themeShadows,
   useTheme,
-} from "../contexts/ThemeContext";
+} from '../contexts/ThemeContext';
 
 const Footer = () => {
   const { theme } = useTheme();
   return (
     <footer
       className="footer"
-      style={{ boxShadow: `0 8px 32px 0 ${themeShadows[theme]}` }}
+      style={{
+        boxShadow: `0 8px 32px 0 ${themeShadows[theme]}`,
+        marginTop: '20px',
+      }}
     >
       <p>Salim Of Shadow KOF</p>
     </footer>
